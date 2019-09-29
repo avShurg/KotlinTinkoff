761c5f94ecaa11ac6250421e12afbdb6ceff5348
Andrew Shurganov <shurganov.av94@gmail.com>
2019-09-29 20:08:30 +0300

    Initial commit

---
+++ src/KotlinTinkoff/InputAndOutput.kt
@@ -0,0 +1,9 @@
+import java.util.*
+
+val scan = Scanner(System.`in`)
+fun main(args: Array<String>) {
+    val name = scan.next()
+    val number1 = scan.nextInt()
+    val number2 = scan.nextInt()
+    println("$name получает $number1 через $number2 лет")
+}
