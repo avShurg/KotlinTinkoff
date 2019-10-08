eadd291bc46c73e9713da3a9ff16cf5a2fae4163
ShurganovAV <shurganov.av94@gmail.com>
2019-10-08 17:29:50 +0300

    Task: "сomparator"

--- src/Main.kt
+++ src/Main.kt
@@ -1,40 +1,26 @@
 import ru.tinkoff.Person
+import ru.tinkoff.PersonComparator
 
 fun main() {
-    val personAndrewSh = Person(
-        "Шурганов",
-        "Андрей",
-        25,
-        390000,
-        "89521299918"
-    )
-    val personDenisP = Person(
-        "Петров",
-        "Денис",
-        23,
-        390001,
-        "89537363214"
-    )
-    val personDenisP2 = Person(
-        "Петров",
-        "Денис",
-        23,
-        390001,
-        "89537363214"
+    val personAndrewSh = Person("Шурганов", "Андрей", 25, 390000, "89521299918")
+    val personDenisP = Person("Петров", "Денис", 23, 390001, "89537363214")
+    val personDenisP2 = Person("Петров", "Денис", 23, 390001, "89537363214")
+    println(
+        if (personAndrewSh.equals(personDenisP)) "${personAndrewSh.firstName} идентичен ${personDenisP.firstName}"
+        else "${personAndrewSh.firstName} не идентичен ${personDenisP.firstName}"
     )
     println(
-        if (personAndrewSh.equals(personDenisP))
-            "${personAndrewSh.firstName} идентичен ${personDenisP.firstName}"
-        else
-            "${personAndrewSh.firstName} не идентичен ${personDenisP.firstName}")
+        if (personAndrewSh.equals(personDenisP2)) "${personAndrewSh.firstName} идентичен ${personDenisP2.firstName}"
+        else "${personAndrewSh.firstName} не идентичен ${personDenisP2.firstName}"
+    )
     println(
-        if (personAndrewSh.equals(personDenisP2))
-            "${personAndrewSh.firstName} идентичен ${personDenisP2.firstName}"
-        else
-            "${personAndrewSh.firstName} не идентичен ${personDenisP2.firstName}")
-    println(
-        if (personDenisP.equals(personDenisP2))
-            "${personDenisP.firstName} идентичен ${personDenisP2.firstName}"
-        else
-            "${personDenisP.firstName} не идентичен ${personDenisP2.firstName}")
+        if (personDenisP.equals(personDenisP2)) "${personDenisP.firstName} идентичен ${personDenisP2.firstName}"
+        else "${personDenisP.firstName} не идентичен ${personDenisP2.firstName}"
+    )
+    println("${personDenisP.age} compare ${personAndrewSh.age} result = ${Person.compare(personDenisP, personAndrewSh)}")
+    println("${personAndrewSh.age} compare ${personDenisP2.age} result = ${Person.compare(personAndrewSh, personDenisP2)}")
+    println("${personDenisP.age} compare ${personDenisP2.age} result = ${Person.compare(personDenisP, personDenisP2)}")
+    println("${personDenisP.age} compare ${personAndrewSh.age} result = ${PersonComparator.compare(personDenisP, personAndrewSh)}")
+    println("${personAndrewSh.age} compare ${personDenisP2.age} result = ${PersonComparator.compare(personAndrewSh, personDenisP2)}")
+    println("${personDenisP.age} compare ${personDenisP2.age} result = ${PersonComparator.compare(personDenisP, personDenisP2)}")
 }
--- src/ru/tinkoff/Person.kt
+++ src/ru/tinkoff/Person.kt
@@ -6,4 +6,36 @@ data class Person(
     val age: Int,
     val postcode: Int,
     val phone: String
-)
+) {
+    companion object : Comparator<Person> {
+        override fun compare(p0: Person?, p1: Person?): Int {
+            return if (p0 != null && p1 != null) {
+                when {
+                    p0.age > p1.age -> 1
+                    p0.age < p1.age -> -1
+                    else -> 0
+                }
+            } else {
+                if (p0 != null && p1 == null) 1
+                else if (p1 != null && p0 == null) -1
+                else 0
+            }
+        }
+    }
+}
+
+object PersonComparator : Comparator<Person> {
+    override fun compare(p0: Person?, p1: Person?): Int {
+        return if (p0 != null && p1 != null) {
+            when {
+                p0.age > p1.age -> 1
+                p0.age < p1.age -> -1
+                else -> 0
+            }
+        } else {
+            if (p0 != null && p1 == null) 1
+            else if (p1 != null && p0 == null) -1
+            else 0
+        }
+    }
+}
