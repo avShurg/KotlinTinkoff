b34258874998252432fd3ea0d652edc569311f2b
ShurganovAV <shurganov.av94@gmail.com>
2019-10-15 13:40:11 +0300

    Task: "Extensions. Task 1"

--- src/ru/tinkoff/Main.kt
+++ src/ru/tinkoff/Main.kt
@@ -1,5 +1,14 @@
 package ru.tinkoff
 
-fun main() {
+fun Pet.isAdult(): Boolean {
+    return this.age >= 2
+}
 
+val Pet.respectableNickname: String
+    get() = "Dear ${this.nickname}"
+
+fun main() {
+    val pet = Pet("Жучка", 2, false)
+    println("Животное совершеннолетние? ${pet.isAdult()}")
+    println(pet.respectableNickname)
 }
---
+++ src/ru/tinkoff/Pet.kt
@@ -0,0 +1,3 @@
+package ru.tinkoff
+
+class Pet(var nickname: String, var age: Int, var sex: Boolean?)
