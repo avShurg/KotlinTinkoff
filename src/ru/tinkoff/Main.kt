9378be22a40119f618ead8fa3b0b8ed87c8c3129
ShurganovAV <shurganov.av94@gmail.com>
2019-10-16 10:13:07 +0300

    Task: "Lesson 6. The function is applied, in addition, let. Fixed remarks"

--- src/ru/tinkoff/Main.kt
+++ src/ru/tinkoff/Main.kt
@@ -1,25 +1,30 @@
 package ru.tinkoff
 
 fun main() {
-    var pet = Pet()
-    pet.apply {
+    val dog = Pet().apply {
         nickname = "pops"
         age = 3
         sex = "man"
     }
-    pet.also {
+    println(dog.nickname)
+    val cat = Pet().also {
         it.nickname = "bob"
         it.age = 1
         it.sex = "women"
     }
-    pet.let {
+    println(cat.nickname)
+    val parrot = Pet().let {
         it.nickname = "kesha"
         it.age = 1
         it.sex = null
+        it
     }
-    with(pet) {
+    println(parrot.nickname)
+    val fish = with(Pet()) {
         nickname = "masha"
         age = 6
         sex = null
+        this
     }
+    println(fish.nickname)
 }
